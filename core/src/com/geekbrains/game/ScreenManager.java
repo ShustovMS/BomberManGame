386c7fc41265ad3ceb48c0ba446b673d0e36da7f
Shustov Mikhail <shustovms@gmail.com>
2018-06-08 23:56:32 +0300

    Добавил очень мало из-за недостатка времени. Также не получилось добавить новый экран (точнее он не заработал, приложение или не компилировалось с ошибкой или зависало). Также почему то не заработал ЛИстенер на кномках на экране паузы, так и не разобрался. Реализовал логику автоматической смены уровней.

---
+++ core/src/com/geekbrains/game/ScreenManager.java
@@ -0,0 +1,98 @@
+package com.geekbrains.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.*;
+
+public class ScreenManager {
+    public enum ScreenType {
+        MENU, GAME, GAME_OVER;
+    }
+
+    private static ScreenManager ourInstance = new ScreenManager();
+
+    public static ScreenManager getInstance() {
+        return ourInstance;
+    }
+
+    private BomberGame game;
+    private Screen targetScreen;
+    private LoadingScreen loadingScreen;
+    private GameScreen gameScreen;
+    private MenuScreen menuScreen;
+    private GameOverScreen gameOverScreen;
+    private SpriteBatch batch;
+    private Viewport viewport;
+    private Camera camera;
+
+    public Viewport getViewport() {
+        return viewport;
+    }
+
+    private ScreenManager() {
+    }
+
+    public void init(BomberGame game, SpriteBatch batch) {
+        this.game = game;
+        this.batch = batch;
+        this.camera = new OrthographicCamera(1280, 720);
+        this.viewport = new FitViewport(1280, 720, camera);
+        this.gameScreen = new GameScreen(batch, camera);
+        this.menuScreen = new MenuScreen(batch);
+        this.loadingScreen = new LoadingScreen(batch);
+        this.gameOverScreen = new GameOverScreen(batch);
+    }
+
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+        viewport.apply();
+    }
+public void game_Over(){
+    Screen screen = game.getScreen();
+    Gdx.input.setInputProcessor(null); // ?
+    Assets.getInstance().clear();
+    if (screen != null) {
+        screen.dispose();
+    }
+    resetCamera();
+    Assets.getInstance().loadAssets(ScreenType.GAME_OVER);
+    game.setScreen(gameOverScreen);
+}
+    public void changeScreen(ScreenType type) {
+        Screen screen = game.getScreen();
+        Gdx.input.setInputProcessor(null); // ?
+        Assets.getInstance().clear();
+        if (screen != null) {
+            screen.dispose();
+        }
+        resetCamera();
+        game.setScreen(loadingScreen);
+        switch (type) {
+            case MENU:
+                targetScreen = menuScreen;
+                Assets.getInstance().loadAssets(ScreenType.MENU);
+                break;
+            case GAME:
+                targetScreen = gameScreen;
+                Assets.getInstance().loadAssets(ScreenType.GAME);
+                break;
+            case GAME_OVER:
+                targetScreen = gameOverScreen;
+                Assets.getInstance().loadAssets(ScreenType.GAME_OVER);
+                break;
+        }
+    }
+
+    public void resetCamera() {
+        camera.position.set(640, 360, 0);
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+    }
+
+    public void goToTarget() {
+        game.setScreen(targetScreen);
+    }
+}
